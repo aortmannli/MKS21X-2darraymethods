ab781cc222aa4a8dbb5b76411b7a963110d474c6
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2018-11-06 09:46:21 -0500

    completed columnSum

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,7 @@
 public class ArrayMethods{
 
   public static int rowSum(int[][] ary, int x){
+    //returns the sum of the elements in Row x of ary.
      if (x >= ary.length) return 0;
      int out = 0;
      for (int i = 0; i < ary[x].length; i++) {
@@ -10,10 +11,19 @@ public class ArrayMethods{
    }
 
     public static int columnSum(int[][] ary, int x){
-
+      //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
+      int out = 0;
+      for (int i = 0; i < ary.length; i++) {
+        if (ary[i].length > x) {
+          out += ary[i][x];
+        }
+      }
+      return out;
     }
 
     public static int[] allRowSums(int[][] ary){
+      //returns an array of the row sums for each row of ary.
+    //Index i of the return array contains the sum of elements in row i.
 
     }
 
